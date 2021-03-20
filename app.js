b338e706c8cfe1ff3ebb0a7e5a9a315beeb65f6c
leslier4 <leslier4@wwu.edu>
2021-03-19 18:31:31 -0700

    minor fix

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 const routes = require('./routes');
 
 app.use('/app', routes);
-
-
-app.get('/projects/marquee/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public_html/projects/marquee/index.html'), (err)=>{
-        if(err){
-            res.status(500).send("i got here");
-        }
-    });
-});
 
 app.listen(port);
